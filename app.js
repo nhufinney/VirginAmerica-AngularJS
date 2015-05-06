7f6609af85ce980387c617bba28c2d933f654ca4
Liz Beacham & Nhu Finney <student@epicodus.com>
2015-05-06 11:51:08 -0700

    Incorporate Firebase db and begin writing logic to write to db

--- app.js
+++ app.js
@@ -3,25 +3,13 @@ var airLine = angular.module('airLine', ['ui.router', 'firebase']);
 airLine.config(function($stateProvider) {
     $stateProvider.state('home', {
         url: "",
-        templateUrl: "partials/home.html",
-        controller: 'FormCtrl'
+        templateUrl: "partials/form.html",
+        controller: 'FormController'
     });
 
-    $stateProvider.state('guests', {
-        url: "/guests_info",
-        templateUrl: "partials/guests.html",
-        controller: 'FormCtrl'
-    });
+    // $stateProvider.state('guests', {
+    //     url: "/guests_info",
+    //     templateUrl: "partials/guests.html",
+    //     controller: 'FormCtrl'
+    // });
 });
-
-
-
-// airLine.directive("guest", function() {
-//     return {
-//         restrict: "E",
-//         scope: {
-//             count: _______
-//         },
-//         template: '<input type="text" ng-model="guestnumber"' + ' {{guestnumber}}' + '<div class="button" ng-click="count({})">Submit</div>'
-//     };
-// });
